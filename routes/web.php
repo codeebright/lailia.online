791b7c89901dbd88b19ce0f650065279a13ca1a8
Hamidullah-hamid <51080654+Hamidullah-hamid@users.noreply.github.com>
2019-09-28 09:31:05 +0430

    route changes

--- routes/web.php
+++ routes/web.php
@@ -12,13 +12,20 @@
 |
 */
 
-<<<<<<< HEAD
+
 // Do login Route
-Route::post('login','OwnerController@login')->name('post_login');
 
-Route::resource('registration', 'OwnerController');
-
-
+// Route::resource('user', 'OwnerController');
+//
+// Route::get('home', function(){
+//   return view('home');
+// });
+Route::post('login')
+Route::resource('home', 'OwnerController');
+// Route::post('registration', 'OwnerController')->name('')
+Route::post('registration', 'OwnerController')->name('registration.create');
+// Route::get('login/create','OwnerController@create')->name('post_login');
+// Route::post('login','OwnerController@login')->name('post_login');
 //Route::get('/', function () {
 //    return view('welcome');
 //});
@@ -67,60 +74,58 @@ Route::resource('registration', 'OwnerController');
 //Route::get('my-home', 'HomeController@myHome');
 //
 //Route::get('my-users', 'HomeController@myUsers');
-=======
-use Illuminate\Support\Facades\Route;
+//
+//use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('adminUser', function () {
-    return view('adminUser');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+//Route::get('adminUser', function () {
+//    return view('adminUser');
+//});
 // Route::get('/demo', function () {
 
 //     return view('demo');
 // });
-
-Route::get('/myHome', function () {
-
-    return view('myHome');
-
-});
-Route::get('/home', function () {
-
-    return view('home');
-
-});
+//
+//Route::get('/myHome', function () {
+//
+//    return view('myHome');
+//
+//});
+//Route::get('/home', function () {
+//
+//    return view('home');
+//
+//});
 // Route::get('/kh_page', function () {
 
 //     return view('kh_page');
 // });
+//
+//Route::get('/khabgah_search', function () {
+//
+//    return view('khabgah_search');
+//
+//});
 
-Route::get('/khabgah_search', function () {
+// khabgah_detailes/khabgah_pages route
+//Route::get('/khabgah_details', [
+//'uses' => 'HostelDetailsController@index',
+//'as'   => 'khabgah_details'
+//]);
 
-    return view('khabgah_search');
-
-});
-
-// khabgah_detailes/khabgah_pages route 
-Route::get('/khabgah_details', [
-'uses' => 'HostelDetailsController@index',
-'as'   => 'khabgah_details'
-]);
-
-
-Route::get('/khabgah_list', function () {
-
-    return view('khabgha_list');
-
-});
-Route::post('adminUser','hostelOwnerController@store');
-
-
-
-Route::get('my-home', 'HomeController@myHome');
-
-Route::get('my-users', 'HomeController@myUsers');
+//
+//Route::get('/khabgah_list', function () {
+//
+//    return view('khabgha_list');
+//
+//});
+//Route::post('adminUser','hostelOwnerController@store');
 
 
->>>>>>> 7bad7c1b79aed32ce97fd724a10adb9974768f3a
+//
+// Route::get('my-home', 'HomeController@myHome');
+//
+// Route::get('my-users', 'HomeController@myUsers');
+//
