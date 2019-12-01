b682fe3139c3d466b4ae4b1a93b57831b948a8d6
Hamidullah-hamid <51080654+Hamidullah-hamid@users.noreply.github.com>
2019-12-01 19:35:50 +0430

    updated routes

--- routes/web.php
+++ routes/web.php
@@ -17,57 +17,21 @@
  *
  * =========================================
  * */
-route::group(['prefix' => 'admin'] , function(){
-    route::get('/hostel/' , 'HostelController@index')->name('hostel.index');
-    route::get('/hostel/create' , 'HostelController@create')->name('hostel.create');
-    route::post('/hostel/create' , 'HostelController@store')->name('hostel.store');
-    Route::get('/hostel/delete/{hostel_id}' ,'HostelController@delete')->name('hostel.delete');
-    route::get('/hostel/edit/{hostel_id}' , 'HostelController@edit')->name('hostel.edit');
-    route::post('/hostel/edit/{hostel_id}' , 'HostelController@update')->name('hostel.update');
-// rooms route .. 'ramazan'
-//    route::get('/room' , 'HostelController@index')->name('hostel.index');
-    route::get('/room/create' , 'RoomController@create')->name('room.create');
-    route::post('/room/create' , 'RoomController@store')->name('room.store');
-    route::get('/room/{id}/delete' , 'RoomController@delete')->name('room.delete');
-    route::get('/room/edit/{id}' , 'RoomController@edit')->name('room.edit');
-    route::post('/room/edit/{id}' , 'RoomController@update')->name('room.update');
 
 // Hoste Resource route Ramazan
-use App\Address;
-
 Route::resource('hostel','HostelController');
 // Food Resource route 'Ramazan'
 Route::resource('food','FoodController');
-
-Route::get('/adminLogin', function () {
-    return view('cms.hostel.login');
-});
-// Hostel Resource route Ramazan
-//Route::resource('hostel','HostelController');
-//dropdown routes create 'ramazan'
-Route::post('hostel/photos' ,'AttachmentController@addphotos')->name('add_photos');
-//photos delete route "ramazan"
-Route::post('hostel/photos/delete' ,'AttachmentController@fileDestroy')->name('fileDestroy');
-//delet the room  ... 'ramazan'
-//Route::get('rooms/delete/{room_id}' ,'RoomController@delete')->name('room.delete');
- //Room resource route 'Ramazan'
-//Route::resource('room','RoomController');
-
-
+// Room resource route 'Ramazan'
+Route::resource('room','RoomController');
 
 Route::get('/test', function () {
     return view('cms.hostel.test');
 });
-//// Route::get('hostel', 'HostelController@index');
-// Route::get('/profile', function(){
-//     return view('cms.hostel.profile');
-// })->name('profile');
-
-
-Route::get('/example', function(){
-    return view('cms.hostel.example');
-})->name('example');
-
+// Route::get('hostel', 'HostelController@index');
+ Route::get('/profile', function(){
+     return view('cms.hostel.profile');
+ })->name('profile');
 
 /*
  * Enf of CMS route
@@ -80,7 +44,7 @@ Route::get('/example', function(){
  * =====================================
  * */
 
-//photo route
+//foto route
 Route::get('/form',function(){
   return view('front/form');
 });
